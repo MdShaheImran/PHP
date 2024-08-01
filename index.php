c8e0ae85d0fa38eb281606ba5737507fe95ca3d2
MdShaheImran <mdshaheimran54@gmail.com>
2024-08-01 10:29:35 +0600

    Variables adn data type

--- index.php
+++ index.php
@@ -6,14 +6,3 @@
     /*this is a multi line
     comment*/
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <button>Click here</button>
-</body>
-</html>
---
+++ variables-data-type.php
@@ -0,0 +1,25 @@
+<?php 
+
+    //Text are string data type 
+    $name= "I'm Md Shahe Imran";
+    $vehical= "Bike";
+    $email= "developerimran@gmail.com";
+    //Text are string data type 
+
+    //Number is integer data type
+    $age=21;
+    $user=2;
+    $quantity=100;
+    //Number is integer data type
+
+    
+
+
+    echo "Hello {$name} <br>";
+    echo "I love {$vehical} <br>";
+    echo " You e-mail is {$email} <br> ";
+    echo "You are {$age} year old <br>";
+    echo "There are {$user} is online <br>";
+    echo "You would like to buy {$quantity} eggs <br>";
+
+?>
