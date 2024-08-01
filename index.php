3730cd8a6123d8b82b09750b827b3fa1ccb63500
MdShaheImran <mdshaheimran54@gmail.com>
2024-08-01 09:36:15 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+    echo " I love PHP <br>";
+    echo "I'm Md Shahe Imran <br>";
+    //This is a comment
+    echo "I've to learn PHP in 7 days <br>";
+    /*this is a multi line
+    comment*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>Click here</button>
+</body>
+</html>
