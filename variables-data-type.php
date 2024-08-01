47dcc5899fd4f1928023e0c5a83aa6e3a6508f85
MdShaheImran <mdshaheimran54@gmail.com>
2024-08-01 12:03:35 +0600

    Last update

--- variables-data-type.php
+++ variables-data-type.php
@@ -12,7 +12,22 @@
     $quantity=100;
     //Number is integer data type
 
+    //This are flot
+    $gpa = 4.00;
+    $price = 99.99;
+    $tax = 9.5;
+    //This are flot
+
+    //This are booleans
+    $employed = true;
+    $online = false;
+    $for_sale = true;
+    //This are booleans
+
     
+    
+
+
 
 
     echo "Hello {$name} <br>";
@@ -21,5 +36,15 @@
     echo "You are {$age} year old <br>";
     echo "There are {$user} is online <br>";
     echo "You would like to buy {$quantity} eggs <br>";
+    echo "You CGPA is {$gpa} <br>";
+    echo "your Pizza is \${$price} <br>";
+    echo "The sales tax rat is {$tax}% <br>";
 
+
+    $total =null;
+    $total = $quantity * $price;
+    echo "You've ordered {$quantity} x {$vehical} <br>";
+    echo "Your total is \${$total} <br>";
+    
+ 
 ?>
